d65c76a30db730792dc49b7932a6b7cd1777e4cd
ishida tsubasa <hiphop19930613@gmail.com>
2020-02-23 17:27:56 +0900

    新規のTweetapp1

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_071246) do
+ActiveRecord::Schema.define(version: 2020_01_23_112150) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
